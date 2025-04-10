018400f737e506badbd5f1be1a4a7f573227487d
Michael Mammoliti <mammoliti.michael@gmail.com>
2025-04-10 04:57:01 +0100

    todos

---
+++ todos.ts
@@ -0,0 +1,14 @@
+{
+  "position": "Senior Developer",
+  "company": "CodeWorks",
+  "duration": "3 years",
+  "expertise_level": "A4",
+  "keywords": ["API development", "machine learning", "cloud"]
+}
+
+const profileID = 1000;
+const keywords = 1-1-1;
+const level = 1-1-1;
+const duration = 1-1-1;
+
+const id = `${profileID}-${keywords}|${level}|${duration}`;
